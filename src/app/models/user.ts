e1db563fec29ea80168cd9e1baab4d49a96c676c
Henrique_Jean <j.darkline.dev@pm.me>
2026-02-24 15:51:49 -0300

    Config-user.ts

--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -1,2 +1,9 @@
 export interface User {
+
+    id:number;
+    name:string;
+    email:string;
+    role: 'Admin' | 'Editor' | 'Viewer';
+    status: 'Active' | 'Inactive';
+    lastLogin: string;
 }
